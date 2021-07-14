943c993eb197507c9d632d22e5159855f72c93f2
Umesh-JNU <umesh98_soe@jnu.ac.in>
2021-07-15 00:06:24 +0530

    sponsors section added

---
+++ src/components/general/sponsors.js
@@ -0,0 +1,55 @@
+import React from "react";
+import img1 from "../../assets/img/sponsors/balsamiq.png";
+import img2 from "../../assets/img/sponsors/twist.png";
+import img3 from "../../assets/img/sponsors/cloudsploit.png";
+import img4 from "../../assets/img/sponsors/devfolio.png";
+import img5 from "../../assets/img/sponsors/doist.png";
+import img6 from "../../assets/img/sponsors/gatsby.svg";
+import img7 from "../../assets/img/sponsors/hackerearth.png";
+import img8 from "../../assets/img/sponsors/hellointern.png";
+import img9 from "../../assets/img/sponsors/matic.svg";
+import img10 from "../../assets/img/sponsors/bugsee.png";
+import { yellow } from "@material-ui/core/colors";
+
+const themes = [
+  { id: "1", side: "left", img: img1 },
+  { id: "2", side: "right", img: img2 },
+  { id: "3", side: "left", img: img3 },
+  { id: "4", side: "right", img: img4 },
+  { id: "5", side: "left", img: img5 },
+  { id: "6", side: "right", img: img6 },
+  { id: "7", side: "left", img: img7 },
+  { id: "8", side: "right", img: img8 },
+  { id: "9", side: "left", img: img9 },
+  { id: "10", side: "right", img: img10 },
+];
+
+function Thm(props) {
+  return (
+    <div className="col-md-4 d-flex justify-content-center" data-aos={props.side === "left" ? "fade-up-left" : "fade-up-right"} key={props.id}>
+      <img
+        src={props.img}
+        className="py-4 img-fluid rounded-start"
+        style={{ maxWidth: "85%", maxHeight: "85%" }}
+        alt="..."
+      />
+    </div>
+  );
+}
+
+export default function Sponsors() {
+  return (
+    <>
+      <hr className="solid" />
+      <h2 className="headingMain2" data-aos="flip-up" style={{backgroundColor: '#c68ad0'}}>Past Sponsors</h2>
+      <br />
+      <div className="container mt-2 mb-2">
+        <div className="card border-0 mb-3 mx-auto">
+          <div className="row g-0 d-flex justify-content-center">
+            {themes.map(theme => Thm(theme))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,7 @@ import Info from "../../components/general/information";
 import Prize from "../../components/general/prize";
 import Timeline from "../../components/general/timeline";
 import Themes from "../../components/general/themes";
+import Sponsors from "../../components/general/sponsors";
 import Jury from "../../components/general/jury";
 import Faqs from "../../components/general/faqs";
 import Footer from "../../components/general/footer";
@@ -16,6 +17,7 @@ const Index = () => {
       <Prize />
       <Timeline />
       <Themes />
+      <Sponsors />
       <Jury />
       <Faqs />
       <Footer />
